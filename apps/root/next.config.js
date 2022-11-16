c9f566d0e574b011d45022eaef4f0198c1ced5bb
sandy.kurniawan <sandy.kurniawan@dataon.com>
2022-11-16 16:24:17 +0700

    set up turborepo

---
+++ apps/root/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+const withTM = require('next-transpile-modules')(['ui']);
+
+module.exports = withTM({
+  nextConfig
+})
