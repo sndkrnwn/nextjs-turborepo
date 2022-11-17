41550567c0af55f8471ccf15e8a7ab7b52371172
sandy.kurniawan <sandy.kurniawan@dataon.com>
2022-11-17 20:37:30 +0700

    add client custom

--- packages/ui/client-custom/client-a/home/index.js
+++ packages/ui/client-custom/client-a/home/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const HomeContent = () => {
     return (
         <React.Fragment>
-            HomeContent
+            Home Client A
         </React.Fragment>
     )   
 }
